e104ba29b143e02c9191adb879bfdd3888d4a29e
ryaneggz <kre8mymedia@gmail.com>
2025-10-17 20:28:13 -0600

    UI in place as should have been before

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,28 +1,29 @@
 #!/usr/bin/env node
-import {render} from 'ink';
-import meow from 'meow';
+import React from 'react';
+import {withFullScreen} from 'fullscreen-ink';
+// import meow from 'meow';
 import App from './app.js';
 
-const cli = meow(
-	`
-	Usage
-	  $ gilfoyle
+// const cli = meow(
+// 	`
+// 	Usage
+// 	  $ gilfoyle
 
-	Options
-		--name  Your name
+// 	Options
+// 		--name  Your name
 
-	Examples
-	  $ gilfoyle --name=Jane
-	  Hello, Jane
-`,
-	{
-		importMeta: import.meta,
-		flags: {
-			name: {
-				type: 'string',
-			},
-		},
-	},
-);
+// 	Examples
+// 	  $ gilfoyle --name=Jane
+// 	  Hello, Jane
+// `,
+// 	{
+// 		importMeta: import.meta,
+// 		flags: {
+// 			name: {
+// 				type: 'string',
+// 			},
+// 		},
+// 	},
+// );
 
-render(<App name={cli.flags.name} />);
+withFullScreen(<App />).start();
